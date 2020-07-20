1da322570a392a855691a38bc609430439b371ce
Leonardo Rodriguez-Velez <leo.r@thedigital.support>
2020-07-20 20:29:18 +0000

    updates

--- README.txt
+++ README.txt
@@ -4,3 +4,7 @@ The file named: Dockerfile in this directory corresponds to the base image "amaz
 The directory ht-sails-app is rsynced from /home/sails/ht-sails-app and is used to create the image 'hero-traveler', which uses the base image above. This is the image that should be used for production.
 
 The file /home/sails/ht-sails-app/Dockerfile is the file that should be modified (in-situ) and committed to git
+
+
+To test the container (with bash):
+docker run --network="host" -it --entrypoint /bin/sh hero-traveler
--- buildSailsApp.sh
+++ buildSailsApp.sh
@@ -6,10 +6,13 @@ echo "Running RSYNC:"
 rsync -Haz --exclude=".git" --exclude=".tmp" --exclude="documentation"  /home/sails/ht-sails-app/ .
 
 echo "Building Dockerfile"
-docker build -t hero-traveler .
+DOCKER_BUILDKIT=1 docker build -t hero-traveler .
 
 echo "Tagging build"
 docker tag hero-traveler:latest 464827642642.dkr.ecr.us-east-1.amazonaws.com/hero-traveler:latest
+
+echo "Authenticating with aws ECR repo:"
+$(aws ecr get-login --no-include-email --region us-east-1)
 
 echo "Pushing image to repo"
 docker push 464827642642.dkr.ecr.us-east-1.amazonaws.com/hero-traveler:latest
