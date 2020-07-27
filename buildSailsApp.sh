dbb73a599130af247a6cbf16d955242678f5d39a
Leonardo Rodriguez-Velez <leo.r@thedigital.support>
2020-07-27 16:34:34 +0000

    update build script with git commit id and branch info

--- buildSailsApp.sh
+++ buildSailsApp.sh
@@ -1,12 +1,23 @@
 #! /bin/bash
 
-cd /home/ec2-user/deploy/ht-sails-app
+git_branch=$(sudo su -c 'git -C /home/sails/ht-sails-app rev-parse --abbrev-ref HEAD')
+git_commit_id="$(sudo su -c 'git -C /home/sails/ht-sails-app log --format="%H" -n 1' sails)"
 
 echo "Running RSYNC:"
-rsync -Haz --exclude=".git" --exclude=".tmp" --exclude="documentation"  /home/sails/ht-sails-app/ .
+rsync -Haz --exclude=".git" --exclude=".tmp" --exclude="documentation"  /home/sails/ht-sails-app/ ./ht-sails-app
+
+touch ./ht-sails-app/gitinfo
+printf "${git_branch}" > ./ht-sails-app/git_branch
+printf "${git_commit_id}" > ./ht-sails-app/git_commit_id
+
+printf "Git branch: "
+cat ./ht-sails-app/git_branch
+printf "\nGit commit id: "
+cat ./ht-sails-app/git_commit_id
+printf "\n"
 
 echo "Building Dockerfile"
-DOCKER_BUILDKIT=1 docker build -t hero-traveler .
+DOCKER_BUILDKIT=1 docker build -t hero-traveler ./ht-sails-app
 
 echo "Tagging build"
 docker tag hero-traveler:latest 464827642642.dkr.ecr.us-east-1.amazonaws.com/hero-traveler:latest
@@ -17,4 +28,3 @@ $(aws ecr get-login --no-include-email --region us-east-1)
 echo "Pushing image to repo"
 docker push 464827642642.dkr.ecr.us-east-1.amazonaws.com/hero-traveler:latest
 
-cd /home/ec2-user/deploy
