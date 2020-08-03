2f60dee7dab1654588493f343f357fe18511992a
Leonardo Rodriguez-Velez <leo.r@thedigital.support>
2020-08-03 18:17:46 +0000

    update base image name to amazon-linux-nodejs13

--- buildBaseImage.sh
+++ buildBaseImage.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
-docker build -f ./Dockerfile-amazon-linux-nodejs12 . && \
+docker build -t amazon-linux-nodejs13 -f ./Dockerfile-amazon-linux-nodejs13 . && \
 
-docker tag amazon-linux-nodejs12:latest 464827642642.dkr.ecr.us-east-1.amazonaws.com/amazon-linux-nodejs12:latest && \
+docker tag amazon-linux-nodejs13:latest 464827642642.dkr.ecr.us-east-1.amazonaws.com/amazon-linux-nodejs13:latest && \
 
 $(aws ecr get-login --no-include-email --region us-east-1) && \
 
-docker push 464827642642.dkr.ecr.us-east-1.amazonaws.com/amazon-linux-nodejs12:latest
+docker push 464827642642.dkr.ecr.us-east-1.amazonaws.com/amazon-linux-nodejs13:latest
